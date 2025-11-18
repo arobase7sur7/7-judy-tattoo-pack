03bdaa85dbd93b796e9e7300a43101d46b8008ff
arobase7sur7 <arobase7sur7@gmail.com>
2025-11-18 10:20:51 +0100

    Add files via upload

---
+++ 7-judy-tattoo-pack/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author '7sur7'
+description 'Tattoo Pack of Judy Alvarez'
+version '1.0.0'
+
+files {
+    'overlays.xml',
+}
+
+data_file 'PED_OVERLAY_FILE' 'overlays.xml'
---
+++ illenium config.lua
@@ -0,0 +1,347 @@
+    ZONE_TORSO = {
+        {
+            name = "judy_spider_breastl",
+            label = "Judy Spider Breast",
+            hashFemale = "judy_spider_breastl_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_spider_breastr",
+            label = "Judy Spider Breast",
+            hashFemale = "judy_spider_breastr_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_sink_breastr",
+            label = "Judy Sink Right",
+            hashFemale = "judy_sink_breastr_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_sinkl",
+            label = "Judy Sink Left",
+            hashFemale = "judy_sinkl_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_truck",
+            label = "Judy Truck",
+            hashFemale = "judy_truck_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_baby",
+            label = "Judy Baby",
+            hashFemale = "judy_baby_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_shark",
+            label = "Judy Shark",
+            hashFemale = "judy_shark_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_from_sea",
+            label = "Judy From Sea",
+            hashFemale = "judy_from_sea_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_seahorsesa",
+            label = "Judy Seahorses A",
+            hashFemale = "judy_seahorsesa_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_seahorsesb",
+            label = "Judy Seahorses B",
+            hashFemale = "judy_seahorsesb_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_cat",
+            label = "Judy Cat",
+            hashFemale = "judy_cat_F",
+            zone = "ZONE_TORSO",
+            collection = "judy_overlays"
+        }
+    }
+    
+    ZONE_LEFT_ARM = {
+        {
+            name = "judy_spider_shoulderl",
+            label = "Judy Spider Shoulder",
+            hashFemale = "judy_spider_shoulderl_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_spider_elbowl",
+            label = "Judy Spider Elbow",
+            hashFemale = "judy_spider_elbowl_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_skulll",
+            label = "Judy Skull",
+            hashFemale = "judy_skulll_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_fearl",
+            label = "Judy Fear",
+            hashFemale = "judy_fearl_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_flower_arml",
+            label = "Judy Flower Arm",
+            hashFemale = "judy_flower_arml_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_flower_wrista",
+            label = "Judy Flower Wrist A",
+            hashFemale = "judy_flower_wrista_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_flower_wristb",
+            label = "Judy Flower Wrist B",
+            hashFemale = "judy_flower_wristb_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_13_arml",
+            label = "Judy 13",
+            hashFemale = "judy_13_arml_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_ghost",
+            label = "Judy Ghost",
+            hashFemale = "judy_ghost_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_moxes",
+            label = "Judy Moxes",
+            hashFemale = "judy_moxes_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_boom",
+            label = "Judy Boom",
+            hashFemale = "judy_boom_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_rosea",
+            label = "Judy Rose A",
+            hashFemale = "judy_rosea_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_roseb",
+            label = "Judy Rose B",
+            hashFemale = "judy_roseb_F",
+            zone = "ZONE_LEFT_ARM",
+            collection = "judy_overlays"
+        }
+    }
+    
+    ZONE_RIGHT_ARM = {
+        {
+            name = "judy_spider_shoulderr",
+            label = "Judy Spider Shoulder",
+            hashFemale = "judy_spider_shoulderr_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_spider_elbowr",
+            label = "Judy Spider Elbow",
+            hashFemale = "judy_spider_elbowr_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_skullr",
+            label = "Judy Skull",
+            hashFemale = "judy_skullr_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_fearr",
+            label = "Judy Fear",
+            hashFemale = "judy_fearr_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_flower_armr",
+            label = "Judy Flower",
+            hashFemale = "judy_flower_armr_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_13_armr",
+            label = "Judy 13",
+            hashFemale = "judy_13_armr_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_wrist_lace",
+            label = "Judy Lace",
+            hashFemale = "judy_wrist_lace_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_dolphin",
+            label = "Judy Dolphin",
+            hashFemale = "judy_dolphin_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_underwater",
+            label = "Judy Underwater",
+            hashFemale = "judy_underwater_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_pulpo",
+            label = "Judy Pulpo",
+            hashFemale = "judy_pulpo_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_techskull",
+            label = "Judy Tech Skull",
+            hashFemale = "judy_techskull_F",
+            zone = "ZONE_RIGHT_ARM",
+            collection = "judy_overlays"
+        }
+    }
+    
+    ZONE_LEFT_LEG = {
+        {
+            name = "judy_lacel",
+            label = "Judy Lace",
+            hashFemale = "judy_lacel_F",
+            zone = "ZONE_LEFT_LEG",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_selfl",
+            label = "Judy Selfmade",
+            hashFemale = "judy_selfl_F",
+            zone = "ZONE_LEFT_LEG",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_dont_like",
+            label = "Judy Don t Like",
+            hashFemale = "judy_dont_like_F",
+            zone = "ZONE_LEFT_LEG",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_lips",
+            label = "Judy Lips",
+            hashFemale = "judy_lips_F",
+            zone = "ZONE_LEFT_LEG",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_feet_spider",
+            label = "Judy Spider",
+            hashFemale = "judy_feet_spider_F",
+            zone = "ZONE_LEFT_LEG",
+            collection = "judy_overlays"
+        }
+    }
+    
+    ZONE_RIGHT_LEG = {
+        {
+            name = "judy_lacer",
+            label = "Judy Lace",
+            hashFemale = "judy_lacer_F",
+            zone = "ZONE_RIGHT_LEG",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_selfr",
+            label = "Judy Selfmade",
+            hashFemale = "judy_selfr_F",
+            zone = "ZONE_RIGHT_LEG",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_owls",
+            label = "Judy Owls",
+            hashFemale = "judy_owls_F",
+            zone = "ZONE_RIGHT_LEG",
+            collection = "judy_overlays"
+        }
+    }
+    
+    ZONE_HEAD = {
+        {
+            name = "judy_flower_necka",
+            label = "Judy Flower A",
+            hashFemale = "judy_flower_necka_F",
+            zone = "ZONE_HEAD",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_flower_neckb",
+            label = "Judy Flower B",
+            hashFemale = "judy_flower_neckb_F",
+            zone = "ZONE_HEAD",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_m_neck",
+            label = "Judy M",
+            hashFemale = "judy_m_neck_F",
+            zone = "ZONE_HEAD",
+            collection = "judy_overlays"
+        },
+        {
+            name = "judy_overcome",
+            label = "Judy Overcome",
+            hashFemale = "judy_overcome_F",
+            zone = "ZONE_HEAD",
+            collection = "judy_overlays"
+        }
+    }
+    
